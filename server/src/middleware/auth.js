f533c0472a392194990a5e7db78fc4b7ffac7d2b
BiTech5 <bikramkhatri050@gmail.com>
2026-05-07 15:08:28 +0545

    auth middleware

---
+++ server/src/middleware/auth.js
@@ -0,0 +1,17 @@
+import jwt, { decode } from "jsonwebtoken";
+
+export default auth=(req,res,next)=>{
+    try{
+        const authHeader=req.headers.authorization;
+        if(!authHeader){
+            return res.status(401).send("No token");
+        }
+        const token=authHeader.split(" ")[1];
+        const decoded=jwt.verify(token, process.env.JWT_SECRET);
+        req.user=decode;
+        next();
+    }catch(err){
+        res.status(401).send(err.message);
+        console.error(err.message);
+    }
+}
