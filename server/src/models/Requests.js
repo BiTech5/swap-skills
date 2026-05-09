f93f717b3a01df7883722845e1da99fbdd161389
BiTech5 <bikramkhatri050@gmail.com>
2026-05-09 15:37:51 +0545

    define schema for  requests

---
+++ server/src/models/Requests.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const requestSchema=new mongoose.Schema(
+    {
+        sender:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:true
+        },
+        receiver:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:true
+        },
+        skill:{
+            type:String,
+            required:true
+        },
+        message:{
+            type:String,
+            default:""
+        },
+        status:{
+            type:String,
+            enum:["pending","accepted","rejected"],
+            default:"pending"
+        }
+    },
+    {
+        timestamps:true
+    }
+);
+
+export default mongoose.model(
+    "Request",
+    requestSchema
+);
