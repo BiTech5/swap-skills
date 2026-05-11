6fc00a26f2aa9cb007f9158aef4519d5e9f6e75b
BiTech5 <bikramkhatri050@gmail.com>
2026-05-11 10:34:59 +0545

    review and rating controller

---
+++ server/src/controllers/reviewControllers.js
@@ -0,0 +1,27 @@
+import Review from "../models/Review.js";
+
+export const createReviews = async (req, res) => {
+  try {
+    const { reviewedUser, rating, comment } = req.body;
+    const review = await Review.create({
+      reviewer: req.user.id,
+      reviewedUser,
+      rating,
+      comment,
+    });
+    res.status(201).json(review);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+export const getUserReview = async (req, res) => {
+  try {
+    const reviews = await Review.find({
+      reviewedUser: req.params.userId,
+    }).populate("reviewer", "name");
+    res.status(200).json(reviews);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
