f5f866e78469ab81907356f70a1c9a664d31dbf7
BiTech5 <bikramkhatri050@gmail.com>
2026-05-11 11:37:42 +0545

    search with query feature

---
+++ server/src/controllers/searchControllers.js
@@ -0,0 +1,19 @@
+import User from "../models/User.js";
+
+export const searchUser=async (req,res)=>{
+    try{
+        const {skill}=req.query.skill;
+        if(!skill){
+            return res.status(400).send("Skill query required");
+        }
+        const users=await User.find({
+            skillsoffered:{
+                $regex:skill,
+                $options:"i"
+            }
+        }).select(-"password");
+        res.status(200).json(users);
+    }catch(err){
+        res.status(500).send(err.message);
+    }
+}
