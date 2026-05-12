d5cfc149e92c3ba73f5fda0170e9bef1da7bc958
BiTech5 <bikramkhatri050@gmail.com>
2026-05-12 17:58:08 +0545

    tailwindcss setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
-    <main style={{ fontFamily: 'system-ui', padding: '2rem' }}>
-      <h1>Swap Skills</h1>
+    <main className="h-screen flex flex-col items-center justify-center">
+      <h1 className="text-red-700 text-4xl font-bold">Swap Skills</h1>
       <p>Platform to teach and learn skills from each other.</p>
     </main>
   );
