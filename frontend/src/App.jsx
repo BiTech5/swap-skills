5f2edd6cfa711d4605867f7cf429fe8b0c99a620
BiTech5 <bikramkhatri050@gmail.com>
2026-05-13 16:11:45 +0545

    add PublicRoute component to manage access to login and registration pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import ProtectedRoute from "./routes/ProtectedRoute";
+import PublicRoute from "./routes/PublicRoute";
 const App = () => {
   return (
     <Routes>
@@ -14,8 +15,17 @@ const App = () => {
           </ProtectedRoute>
         }
       />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={
+        <PublicRoute>
+          <Login />
+        </PublicRoute>
+      }
+      />
+      <Route path="/register" element={
+        <PublicRoute>
+          <Register />
+        </PublicRoute>
+    } />
     </Routes>
   );
 };
