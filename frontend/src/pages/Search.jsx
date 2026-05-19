fc227004e25bbc0df6fb69a3e9a5c13e022e6048
BiTech5 <bikramkhatri050@gmail.com>
2026-05-19 17:56:51 +0545

    search page ui

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import api from "../api/api";
+import Navbar from "../components/Navbar";
 const Search = () => {
-  return <>Search page</>;
+  const [skill, setSkill] = useState("");
+  const { data, refetch, isLoading } = useQuery({
+    queryKey: ["search-users"],
+    queryFn: async () => {
+      const res = await api.get(`/search?skill=${skill}`);
+      return res.data;
+    },
+    enabled: false,
+  });
+  const handleSearch = () => {
+    if (!skill.trim()) return;
+    refetch();
+  };
+  return (
+    <>
+      <Navbar />
+      <div className="p-5">
+        <div>
+            <input type="text" />
+            <button>Search</button>
+        </div>
+        {!isLoading &&(
+            <p>Loading...</p>
+        )}
+
+        <div>
+            {data?.map((user)=>{
+                <div>
+                    <h2>{user.name}</h2>
+                    <p>{user.bio}</p>
+                    <div>
+                        {user.skillsOffered?.map(skill=>{
+                            <span>{skill}</span>
+                        })}
+                    </div>
+                </div>
+            })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Search;
